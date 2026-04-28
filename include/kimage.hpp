a1ed76e18bc2edbedd01806069cb8c10ae71dd49
KatzaShork <114446618+KatzaCat@users.noreply.github.com>
2026-04-28 16:52:00 -0500

    updated how the image takes dependencies

--- include/kimage.hpp
+++ include/kimage.hpp
@@ -8,9 +8,12 @@ namespace k {
         class Image {
         public:
                 Image();
+                Image(ImageReader *reader);
                 Image(const std::string file, ImageReader *reader);
 
-                bool load(const std::string file, ImageReader *reader);
+                bool load(const std::string file);
+
+                bool setReder(ImageReader *reader);
 
                 image::DataResopnce getData();
         private:
--- src/kimage.cpp
+++ src/kimage.cpp
@@ -2,16 +2,22 @@
 
 k::Image::Image() {}
 
+k::Image::Image(ImageReader *reader)
+{if (!this->setReder(reader)) {return;}}
+
 k::Image::Image(const std::string file, ImageReader *reader)
-{this->load(file, reader);}
+{this->load(file);}
 
-bool k::Image::load(const std::string file, ImageReader *reader) {
-        this->reader = reader;
-        if (this->reader == nullptr) {return false;}
-
+bool k::Image::load(const std::string file) {
         bool error_value = this->reader->load(file);
         if (!error_value) {return false;}
 
+        return true;
+}
+
+bool k::Image::setReder(ImageReader *reader) {
+        this->reader = reader;
+        if (this->reader == nullptr) {return false;}
         return true;
 }
 
