ca17504638a591e7f52454f5c4ae92c7d4a581ca
KatzaShork <114446618+KatzaCat@users.noreply.github.com>
2026-04-28 16:10:41 -0500

    filled Image class

--- include/kimage.hpp
+++ include/kimage.hpp
@@ -1,6 +1,19 @@
 #pragma once
 
+#include <string>
+
+#include "imagereader.hpp"
+
 namespace k {
         class Image {
+        public:
+                Image();
+                Image(const std::string file, ImageReader *reader);
+
+                bool load(const std::string file, ImageReader *reader);
+
+                image::DataResopnce getData();
+        private:
+                ImageReader *reader = nullptr;
         };
 }
--- src/kimage.cpp
+++ src/kimage.cpp
@@ -1,1 +1,19 @@
 #include "kimage.hpp"
+
+k::Image::Image() {}
+
+k::Image::Image(const std::string file, ImageReader *reader)
+{this->load(file, reader);}
+
+bool k::Image::load(const std::string file, ImageReader *reader) {
+        this->reader = reader;
+        if (this->reader == nullptr) {return false;}
+
+        bool error_value = this->reader->load(file);
+        if (!error_value) {return false;}
+
+        return true;
+}
+
+k::image::DataResopnce k::Image::getData()
+{return this->reader->getData();}
