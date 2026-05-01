23d25b426ab2d32b0bcc2c7d87d71002c0757e78
KatzaShork <114446618+KatzaCat@users.noreply.github.com>
2026-05-01 00:25:20 -0500

    TEMP
    Started working on actually loading the bmp file

--- include/imagereader/bmp.hpp
+++ include/imagereader/bmp.hpp
@@ -1,14 +1,40 @@
 #pragma once
 
 #include "../imagereader.hpp"
+#include <cstddef>
 
 namespace k {
         class BMPReader : public ImageReader {
         public:
                 virtual ~BMPReader() override = default;
 
-                virtual bool load(const std::string file) override;
+                virtual bool load(const std::string file_name) override;
 
                 virtual image_data::Resopnce getData() override;
+
+        private:
+                size_t number_of_colors = 0;
+
+        private:
+                bool proccessDataBffer(std::vector<std::byte> data_buffer);
+
+
+                const size_t SIGNATURE_POSITION       = 2;
+                const size_t FILESIZE_POSITION        = 4;
+                const size_t DATAOFFSET_POSITION      = 4;
+                const size_t SIZE_POSITION            = 4;
+                const size_t WIDTH_POSITION           = 4;
+                const size_t HEIGHT_POSITION          = 4;
+                const size_t PLANES_POSITION          = 2;
+                const size_t BITSPERPIXEL_POSITION    = 2;
+                const size_t COMPRESSION_POSITION     = 4;
+                const size_t IMAGESIZE_POSITION       = 4;
+                const size_t XPIXELSPERM_POSITION     = 4;
+                const size_t YPIXELSPERM_POSITION     = 4;
+                const size_t COLORSUSED_POSITION      = 4;
+                const size_t IMPORTANTCOLORS_POSITION = 4;
+                const size_t RED_POSITION             = 1;
+                const size_t GREEN_POSITION           = 1;
+                const size_t BLUE_POSITION            = 1;
         };
 }
--- src/imagereader/bmp.cpp
+++ src/imagereader/bmp.cpp
@@ -2,11 +2,30 @@
 #include "imagereader.hpp"
 
 #include <cstddef>
+#include <fstream>
+#include <ios>
 #include <print>
 #include <string>
+#include <vector>
 
-bool k::BMPReader::load(const std::string file) {
-        std::println("TODO: BMP load");
+bool k::BMPReader::load(const std::string file_name) {
+        std::ifstream file(file_name, std::ios::binary);
+        if (!file.is_open()) {return false;}
+
+        std::streamsize file_stream_size = file.tellg();
+        file.seekg(0, std::ios::beg);
+
+        std::vector<std::byte> data_buffer(file_stream_size);
+        if (!file.read(reinterpret_cast<char *>(data_buffer.data()), file_stream_size)) {
+                file.close();
+                return false;
+        }
+        if (!this->proccessDataBffer(data_buffer)) {
+                file.close();
+                return false;
+        }
+
+        file.close();
 
         return true;
 }
@@ -19,4 +38,32 @@ k::image_data::Resopnce k::BMPReader::getData() {
         responce.as.data = std::byte(1);
 
         return responce;
+}
+
+bool k::BMPReader::proccessDataBffer(std::vector<std::byte> data_buffer) {
+        // Header (14 bytes) :
+        //      Signature       -> 2 bytes
+        //      FileSize        -> 4 bytes
+        //      Reserved        -> 4 bytes
+        //      DataOffset      -> 4 bytes
+        // InfoHeader (40) :
+        //      Size            -> 4 bytes
+        //      Width           -> 4 bytes
+        //      Height          -> 4 bytes
+        //      Planes          -> 2 bytes
+        //      BitsPerPixel    -> 2 bytes
+        //      Compression     -> 4 bytes
+        //      ImageSize       -> 4 bytes
+        //      XpixelsPerM     -> 4 bytes
+        //      YpixelsPerM     -> 4 bytes
+        //      ColorsUsed      -> 4 bytes
+        //      ImportantColors -> 4 bytes
+        // ColorTable (4 * number_of_colors bytes) :
+        //      Red             -> 1 byte
+        //      Green           -> 1 byte
+        //      Blue            -> 1 byte
+        //      Reserved        -> 1 byte
+        // PixelData ()
+
+        return true;
 }
