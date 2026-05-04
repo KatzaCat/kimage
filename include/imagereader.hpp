ea9512595c37a6b37166a4c0e429a75e0ca9537e
KatzaShork <114446618+KatzaCat@users.noreply.github.com>
2026-05-03 20:27:31 -0500

    updated data collectors

--- include/imagereader.hpp
+++ include/imagereader.hpp
@@ -2,6 +2,7 @@
 
 #include <cstddef>
 #include <fstream>
+#include <memory>
 #include <string>
 #include <variant>
 #include <vector>
@@ -15,14 +16,13 @@ namespace k {
 
                 struct Response {
                         Type type;
-                        std::variant<std::vector<std::byte>, int> information;
+                        std::variant<std::unique_ptr<std::byte>, int> information;
                 };
         }
 
         class ImageReader {
         public:
                 std::vector<std::byte> data;
-
         public:
                 virtual ~ImageReader() = default;
 
@@ -31,8 +31,17 @@ namespace k {
                 virtual image_data::Response getData() = 0;
 
                 virtual void printData() = 0;
-
+        private:
+                size_t data_index = 0;
         protected:
                 bool populateDataFromFile(std::ifstream &file);
+
+                std::byte getCurrentByte();
+
+                uint16_t getCurrentTwoBytesBigEndian();
+                uint32_t getCurrentFourBytesBigEndian();
+
+                uint16_t getCurrentTwoBytesLittleEndian();
+                uint32_t getCurrentFourBytesLittleEndian();
         };
 }
