b6ce0ca1a8bd601aec22ec811c54ccda68aabfb2
KatzaShork <114446618+KatzaCat@users.noreply.github.com>
2026-05-03 20:27:01 -0500

    remade data colectors

--- src/imagereader.cpp
+++ src/imagereader.cpp
@@ -1,5 +1,6 @@
 #include "imagereader.hpp"
 #include <cstddef>
+#include <cstdint>
 
 bool k::ImageReader::populateDataFromFile(std::ifstream &file) {
         file.seekg(0, std::ios::end);
@@ -17,4 +18,42 @@ bool k::ImageReader::populateDataFromFile(std::ifstream &file) {
         }
 
         return true;
+}
+
+std::byte k::ImageReader::getCurrentByte() {
+        std::byte current_byte = std::byte(0);
+
+        if (this->data_index > this->data.size())
+        {return std::byte(0);}
+
+        current_byte = this->data.at(this->data_index);
+        this->data_index += 1;
+
+        return current_byte;
+}
+
+uint16_t k::ImageReader::getCurrentTwoBytesBigEndian() {
+        return  static_cast<uint16_t>(this->getCurrentByte()) << 8 |
+                static_cast<uint16_t>(this->getCurrentByte());
+}
+
+uint32_t k::ImageReader::getCurrentFourBytesBigEndian() {
+        return  static_cast<uint32_t>(this->getCurrentTwoBytesBigEndian()) << 16 |
+                static_cast<uint32_t>(this->getCurrentTwoBytesBigEndian());
+}
+
+uint16_t k::ImageReader::getCurrentTwoBytesLittleEndian() {
+        std::byte first_byte = this->getCurrentByte();
+        std::byte second_byte = this->getCurrentByte();
+
+        return  static_cast<uint16_t>(second_byte) << 8 |
+                static_cast<uint16_t>(first_byte);
+}
+
+uint32_t k::ImageReader::getCurrentFourBytesLittleEndian() {
+        uint16_t front_bytes = this->getCurrentTwoBytesLittleEndian();
+        uint16_t back_bytes = this->getCurrentTwoBytesLittleEndian();
+
+        return  static_cast<uint32_t>(back_bytes) << 16 |
+                static_cast<uint32_t>(front_bytes);
 }
