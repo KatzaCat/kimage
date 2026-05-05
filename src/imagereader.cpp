23b6bab834038b902c3025ad985d5918cd76d28f
KatzaShork <114446618+KatzaCat@users.noreply.github.com>
2026-05-04 21:59:52 -0500

    updated byte reading to use new int_type tool

--- src/imagereader.cpp
+++ src/imagereader.cpp
@@ -1,4 +1,5 @@
 #include "imagereader.hpp"
+#include "tools/int_type.hpp"
 #include <cstddef>
 #include <cstdint>
 
@@ -26,28 +27,26 @@ std::byte k::ImageReader::getByte(size_t offset) {
         return this->data.at(offset);
 }
 
-uint16_t k::ImageReader::getTwoBytesBigEndian(size_t offset) {
-        return  static_cast<uint16_t>(this->getByte(offset)) << 8 |
-                static_cast<uint16_t>(this->getByte(offset + 1));
-}
+uint16_t k::ImageReader::getTwoBytesBigEndian(size_t offset)
+{return tools::toInt16(this->getByte(offset), this->getByte(offset + 1));}
 
 uint32_t k::ImageReader::getFourBytesBigEndian(size_t offset) {
-        return  static_cast<uint32_t>(this->getTwoBytesBigEndian(offset)) << 16 |
-                static_cast<uint32_t>(this->getTwoBytesBigEndian(offset + 2));
+        return  tools::toInt32(
+                this->getFourBytesBigEndian(offset),
+                this->getFourBytesBigEndian(offset + 2)
+        );
 }
 
 uint16_t k::ImageReader::getTwoBytesLittleEndian(size_t offset) {
         std::byte first_byte = this->getByte(offset);
         std::byte second_byte = this->getByte(offset + 1);
 
-        return  static_cast<uint16_t>(second_byte) << 8 |
-                static_cast<uint16_t>(first_byte);
+        return tools::toInt16(second_byte, first_byte);
 }
 
 uint32_t k::ImageReader::getFourBytesLittleEndian(size_t offset) {
         uint16_t front_bytes = this->getTwoBytesLittleEndian(offset);
         uint16_t back_bytes = this->getTwoBytesLittleEndian(offset + 2);
 
-        return  static_cast<uint32_t>(back_bytes) << 16 |
-                static_cast<uint32_t>(front_bytes);
+        return  tools::toInt32(back_bytes, front_bytes);
 }
