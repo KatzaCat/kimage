db99c0dff29fda538a47da1e79fb0d688c119f94
KatzaShork <114446618+KatzaCat@users.noreply.github.com>
2026-05-04 21:01:53 -0500

    grabbed only the needed information

--- include/imagereader/bmp.hpp
+++ include/imagereader/bmp.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../imagereader.hpp"
+#include <cstdint>
 
 namespace k {
         class BMPReader : public ImageReader {
@@ -12,5 +13,19 @@ namespace k {
                 virtual image_data::Response getData() override;
 
                 virtual void printData() override;
+
+                virtual int32_t getWidth() override;
+                virtual int32_t getHeight() override;
+        private:
+                image_data::Response response = {};
+
+                uint32_t pixel_array_offset = 0;
+
+                int32_t image_width = 0;
+                int32_t image_height = 0;
+
+                uint16_t bits_per_pixel = 0;
+        private:
+                void populateBMPData();
         };
 }
--- src/imagereader/bmp.cpp
+++ src/imagereader/bmp.cpp
@@ -4,7 +4,6 @@
 #include <cstddef>
 #include <fstream>
 #include <ios>
-#include <memory>
 #include <print>
 #include <string>
 #include <vector>
@@ -16,6 +15,7 @@ bool k::BMPReader::load(const std::string file_name) {
         {return false;}
 
         this->populateDataFromFile(binary_file);
+        this->populateBMPData();
 
         binary_file.close();
 
@@ -23,15 +23,38 @@ bool k::BMPReader::load(const std::string file_name) {
 }
 
 k::image_data::Response k::BMPReader::getData() {
-        std::println("TODO: BMP getData");
+        this->response.type = image_data::DATA;
+        this->response.information = this->data;
 
-        image_data::Response response;
-        response.type = image_data::DATA;
-
-        std::unique_ptr<std::byte> data_pointer(this->data.data());
-        response.information = std::move(data_pointer);
-
-        return response;
+        return this->response;
 }
 
-void k::BMPReader::printData() {}
+void k::BMPReader::printData() {
+        for (size_t index = 0; index < this->data.size(); index++) {
+                std::print("{:02X} ", static_cast<unsigned char>(this->data.at(index)));
+
+                if (((index + 1) % 20) == 0)
+                {std::print("\n");}
+        }
+        std::print("\n");
+
+        std::println("Pixel Array Offset : {:8} -> 0x{:08X}", this->pixel_array_offset, this->pixel_array_offset);
+        std::println("Image Width        : {:8} -> 0x{:08X}", this->image_width, this->image_width);
+        std::println("Image Height       : {:8} -> 0x{:08X}", this->image_height, this->image_height);
+        std::println("Bits Per Pixel     : {:8} -> 0x{:08X}", this->bits_per_pixel, this->bits_per_pixel);
+}
+
+int32_t k::BMPReader::getWidth()
+{return this->image_width;}
+
+int32_t k::BMPReader::getHeight()
+{return this->image_height;}
+
+void k::BMPReader::populateBMPData() {
+        this->pixel_array_offset = this->getFourBytesLittleEndian(10);
+
+        this->image_width = this->getFourBytesLittleEndian(18);
+        this->image_height = this->getFourBytesLittleEndian(22);
+
+        this->bits_per_pixel = this->getTwoBytesLittleEndian(28);
+}
