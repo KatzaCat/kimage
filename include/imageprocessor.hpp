a9ee85ea0ff9d12312118e3488c46c446296520c
KatzaShork <114446618+KatzaCat@users.noreply.github.com>
2026-05-15 14:18:21 -0500

    made ImageProcessor purely virtual

--- include/imageprocessor.hpp
+++ include/imageprocessor.hpp
@@ -1,34 +1,19 @@
 #pragma once
 
-#include <cstddef>
 #include <cstdint>
-#include <fstream>
 #include <string>
 #include <vector>
 
 namespace k {
         class ImageProcessor {
         public:
-                std::vector<std::byte> byte_data = {};
-                std::vector<uint32_t> pixel_data = {};
-        public:
                 virtual ~ImageProcessor() = default;
 
                 virtual bool load(const std::string file_name) = 0;
 
-                std::vector<unsigned char> getData();
+                virtual std::vector<unsigned char> getData() = 0;
 
                 virtual int32_t getWidth() = 0;
                 virtual int32_t getHeight() = 0;
-        protected:
-                bool populateDataFromFile(std::ifstream &file);
-
-                std::byte getByte(size_t offset);
-
-                uint16_t getTwoBytesBigEndian(size_t offset);
-                uint32_t getFourBytesBigEndian(size_t offset);
-
-                uint16_t getTwoBytesLittleEndian(size_t offset);
-                uint32_t getFourBytesLittleEndian(size_t offset);
         };
 }
