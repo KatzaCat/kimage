b4c8a65e0237c875d14e36f160fc4c41279cc0ff
KatzaShork <114446618+KatzaCat@users.noreply.github.com>
2026-05-17 17:55:56 -0500

    added chunk loading

--- src/imageprocessor/png.cpp
+++ src/imageprocessor/png.cpp
@@ -1,6 +1,9 @@
 #include "imageprocessor/png.hpp"
 
+#include <cstddef>
+#include <cstdint>
 #include <fstream>
+#include <print>
 
 bool k::PNGProcessor::load(const std::string file_name) {
         std::ifstream binary_file(file_name, std::ios::binary);
@@ -9,6 +12,8 @@ bool k::PNGProcessor::load(const std::string file_name) {
         {return false;}
 
         this->data.populateFromFile(binary_file);
+        if (!this->loadChunks())
+        {return false;}
 
         binary_file.close();
 
@@ -20,7 +25,27 @@ std::vector<unsigned char> k::PNGProcessor::getData() {
 }
 
 int32_t k::PNGProcessor::getWidth()
-{return 0;}
+{return this->width;}
 
 int32_t k::PNGProcessor::getHeight()
-{return 0;}
+{return this->height;}
+
+bool k::PNGProcessor::loadChunks() {
+        png::Chunk chunk = {};
+
+        for (size_t index = 8; index < this->data.getByteArraySize(); index += 12 + chunk.length) {
+                chunk = {};
+
+                chunk.length = this->data.getFourBytesBigEndian(index);
+                chunk.type = this->data.getFourBytesBigEndian(index + 4);
+
+                for (size_t data_index = 0; data_index < chunk.length; data_index++)
+                {chunk.data.push_back(this->data.getByte((index + 8) + data_index));}
+
+                chunk.crc = this->data.getFourBytesBigEndian((index + 8) + chunk.length);
+
+                this->chunks.push_back(chunk);
+        }
+
+        return true;
+}
