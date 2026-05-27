0d36d5b6f3e5a3f6b4276350002f9216a886f44a
KatzaShork <114446618+KatzaCat@users.noreply.github.com>
2026-05-26 23:34:17 -0500

    Update png_loading.cpp

--- test/png_loading.cpp
+++ test/png_loading.cpp
@@ -1,8 +1,12 @@
 #include "kimage.hpp"
+#include <print>
 
 int main() {
         k::Image png_image;
         png_image.load("images/PNGImage.png");
+
+        std::println("width  : {}", png_image.getWidth());
+        std::println("height : {}", png_image.getHeight());
 
         return 0;
 }
