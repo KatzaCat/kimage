c5f39a5f29240e6f1b82bbba78364b9999ed5384
KatzaShork <114446618+KatzaCat@users.noreply.github.com>
2026-05-30 15:52:50 -0500

    Starting from scratch

--- include/imageprocessor/png.hpp
+++ include/imageprocessor/png.hpp
@@ -1,23 +1,10 @@
 #include "imagedata.hpp"
 #include "imageprocessor.hpp"
+#include <cstddef>
 #include <cstdint>
 #include <vector>
 
-#define K_PNG_INFORMATION_HEADER 0x49484452
-#define K_PNG_PALETTE            0x504C5445
-#define K_PNG_IMAGE_DATA         0x49444154
-#define K_PNG_IMAGE_END          0x49454E44
-
 namespace k {
-        namespace png {
-                struct Chunk {
-                        uint32_t length;
-                        uint32_t type;
-                        std::vector<std::byte> data;
-                        uint32_t crc;
-                };
-        }
-
         class PNGProcessor : public ImageProcessor {
         public:
                 virtual ~PNGProcessor() = default;
@@ -28,14 +15,9 @@ namespace k {
 
                 virtual int32_t getWidth() override;
                 virtual int32_t getHeight() override;
+
+                virtual std::string getName() override;
         private:
                 ImageData data = {};
-                int32_t width = 0;
-                int32_t height = 0;
-
-                std::vector<png::Chunk> chunks;
-
-        private:
-                bool loadChunks();
         };
 }
--- src/imageprocessor/png.cpp
+++ src/imageprocessor/png.cpp
@@ -1,18 +1,11 @@
 #include "imageprocessor/png.hpp"
 
-#include <cstddef>
-#include <cstdint>
 #include <fstream>
-#include <print>
 
 bool k::PNGProcessor::load(const std::string file_name) {
         std::ifstream binary_file(file_name, std::ios::binary);
 
         if (!binary_file.is_open())
-        {return false;}
-
-        this->data.populateFromFile(binary_file);
-        if (!this->loadChunks())
         {return false;}
 
         binary_file.close();
@@ -25,27 +18,10 @@ std::vector<unsigned char> k::PNGProcessor::getData() {
 }
 
 int32_t k::PNGProcessor::getWidth()
-{return this->width;}
+{return 0;}
 
 int32_t k::PNGProcessor::getHeight()
-{return this->height;}
+{return 0;}
 
-bool k::PNGProcessor::loadChunks() {
-        png::Chunk chunk = {};
-
-        for (size_t index = 8; index < this->data.getByteArraySize(); index += 12 + chunk.length) {
-                chunk = {};
-
-                chunk.length = this->data.getFourBytesBigEndian(index);
-                chunk.type = this->data.getFourBytesBigEndian(index + 4);
-
-                for (size_t data_index = 0; data_index < chunk.length; data_index++)
-                {chunk.data.push_back(this->data.getByte((index + 8) + data_index));}
-
-                chunk.crc = this->data.getFourBytesBigEndian((index + 8) + chunk.length);
-
-                this->chunks.push_back(chunk);
-        }
-
-        return true;
-}
+std::string k::PNGProcessor::getName() 
+{return "PNG Processor";}
